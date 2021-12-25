afbec47c15c404053fd64764e16f9eeced8ccd8b
Sky Z <qe201020335@sina.com>
2021-12-25 01:23:10 -0500

    we do some manifest editing

--- RankedPlaylist/Properties/AssemblyInfo.cs
+++ RankedPlaylist/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RankedPlaylist")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generate a playlist of ranked songs base on options")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RankedPlaylist")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
