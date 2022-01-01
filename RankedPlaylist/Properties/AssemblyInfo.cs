ee6bea9f5657ca1c0507305f43b030b614a6f50f
Sky Z <qe201020335@sina.com>
2022-01-01 18:07:36 -0500

    copyright 2022 :D

--- RankedPlaylist/Properties/AssemblyInfo.cs
+++ RankedPlaylist/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RankedPlaylist")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © qe201020335 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- RankedPlaylistGenerator/Properties/AssemblyInfo.cs
+++ RankedPlaylistGenerator/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RankedPlaylistGenerator")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © qe201020335 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
