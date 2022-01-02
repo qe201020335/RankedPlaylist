1912196eb8eefdc3c9611914b098d24f0372fb2c
Sky Z <qe201020335@sina.com>
2022-01-02 03:20:02 -0500

    new filters on standalone

--- RankedPlaylistGenerator/Program.cs
+++ RankedPlaylistGenerator/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using RankedPlaylist.RankedPlaylistGenerator.Events;
+using RankedPlaylist.RankedPlaylistGenerator.Utils;
 using ErrorEventArgs = RankedPlaylist.RankedPlaylistGenerator.Events.ErrorEventArgs;
 
 namespace RankedPlaylistGenerator
@@ -36,32 +37,118 @@ namespace RankedPlaylistGenerator
 	        i = result;
 	        return success;
         }
+        
+        private static bool ScanBool(out bool b, string prompt)
+        {
+	        Console.Write(prompt);
+	        var result = Console.ReadLine();
+	        if (result == null)
+	        {
+		        b = false;
+		        return true;
+	        }
+
+	        if (result.ToLower() == "y")
+	        {
+		        b = true;
+		        return true;
+	        }
+	        if (result.ToLower() == "n")
+	        {
+		        b = false;
+		        return true;
+	        }
+
+	        b = false;
+	        return false;
+        }
 
         public static async Task Main(string[] args)
         {
 	        float minStar;
 	        float maxStar;
 	        int size;
-			
-	        while (!ScanFloat(out maxStar, "Maximum Star:"))
+
+	        Filter filter;
+	        bool byAcc = false;
+	        float acc = 1;
+	        string id = "";
+	        
+
+	        while (!ScanFloat(out maxStar, "Maximum Star: "))
 	        {
 		        Console.WriteLine("Invalid input");
 	        }
 
-	        while (!ScanFloat(out minStar, "Minimum Star:"))
+	        while (!ScanFloat(out minStar, "Minimum Star: "))
+	        {
+		        Console.WriteLine("Invalid input");
+	        }
+
+	        bool onlyUnplayed = false;
+	        bool onlyPlayed = false;
+
+	        while (!ScanBool(out onlyUnplayed, "Only include maps you have not played? (y/n) "))
 	        {
 		        Console.WriteLine("Invalid input");
 	        }
 	        
-	        while (!ScanInt(out size, "How Many? (roughly):"))
+	        if (!onlyUnplayed)
+	        {
+		        while (!ScanBool(out onlyPlayed, "Only include maps you have played? (y/n) "))
+		        {
+			        Console.WriteLine("Invalid input");
+		        }
+	        }
+
+	        if (onlyUnplayed)
+	        {
+		        filter = Filter.UnplayedOnly;
+	        }
+	        else if (onlyPlayed)
+	        {
+		        filter = Filter.PlayedOnly;
+	        }
+	        else
+	        {
+		        filter = Filter.Both;
+	        }
+
+	        if (filter != Filter.UnplayedOnly)
+	        {
+		        while (!ScanFloat(out acc, "Target accuracy on played maps (Enter a negative number to include all): "))
+		        {
+			        Console.WriteLine("Invalid input");
+		        }
+		        
+		        byAcc = !(acc < 0);
+
+		        if (acc > 2f)
+		        {
+			        // there is probably no way of getting a acc of 200% (with all the positive modifiers)
+			        // we want to use decimal instead of percentage. i.e, 0.95 instead of 95(%)
+			        acc /= 100;
+		        }
+	        }
+
+	        if (filter != Filter.Both || byAcc)
+	        {
+		        Console.Write("Your ScoreSaber profile ID: ");
+		        id = Console.ReadLine();
+	        }
+	        
+	        while (!ScanInt(out size, "How Many?: "))
 	        {
 		        Console.WriteLine("Invalid input");
 	        }
+	        
 	        
 	        var filename = $"__RankedPlaylist_generated_{DateTimeOffset.Now.ToUnixTimeSeconds()} {minStar}-{maxStar}";
 	        var generator = new RankedPlaylist.RankedPlaylistGenerator.RankedPlaylistGenerator(minStar, maxStar, size, filename);
 	        generator.OnError += OnError;
 	        generator.OnSongAdd += OnSongAdd;
+	        
+	        generator.SetFilters(filter, id, byAcc, acc);
 
 	        var count = await generator.Make();
 	        
